fe2518b2dfbeffc45f8875a795d017575f276798
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2024-09-19 17:42:55 +0530

    Time: 0 ms (100%), Space: 12.5 MB (81.74%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+// void solve(int ind,int target,vector<int>&candidates,vector<vector<int>>&ans,vector<int>ds){
+//    if(ind==candidates.size()){
+//     if(target==0){
+//         ans.push_back(ds);
+//     }
+//         return;
+//    }
+//    //pick up an element
+//    if(candidates[ind]<=target){
+//     ds.push_back(candidates[ind]);
+//     solve(ind,target-candidates[ind],candidates,ans,ds);
+//    }
+//     solve(ind+1,target,candidates,ans,ds);
+   
+// }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<vector<int>>ans;
+//         vector<int>ds;
+//         solve(0,target,candidates,ans,ds);
+//         return ans;
+//     }
+
+
+
+void solve(int ind, int target, vector<int>& candidates, vector<vector<int>>& ans, vector<int>& ds) {
+    // Base case: If the target is reached, store the current combination
+    if (target == 0) {
+        ans.push_back(ds);
+        return;
+    }
+
+    // Recursive case: Explore each candidate starting from 'ind'
+    for (int i = ind; i < candidates.size(); i++) {
+        if (candidates[i] <= target) {
+            // Choose the current candidate
+            ds.push_back(candidates[i]);
+
+            // Recurse with the same candidate since it can be used again
+            solve(i, target - candidates[i], candidates, ans, ds);
+
+            // Backtrack: remove the last added candidate
+            ds.pop_back();
+        }
+    }
+}
+
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> ans;
+    vector<int> ds;
+    solve(0, target, candidates, ans, ds);
+    return ans;
+}
+
+};
