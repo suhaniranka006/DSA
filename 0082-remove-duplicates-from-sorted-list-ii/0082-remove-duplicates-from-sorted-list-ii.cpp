c06bfba6f3b5a34e215fe3c434d164172feb40a7
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2024-09-27 13:11:49 +0530

    Time: 3 ms (88.94%), Space: 15.4 MB (94.89%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -15,34 +15,65 @@ public:
 
       
 
-    map<int,int>mp;
+    // map<int,int>mp;
 
-    ListNode* p=head;
+    // ListNode* p=head;
 
-    while(p){
-        mp[p->val]++;
-        p=p->next;
-    }
+    // while(p){
+    //     mp[p->val]++;
+    //     p=p->next;
+    // }
 
-    ListNode *head2=NULL,*tail=NULL;
+    // ListNode *head2=NULL,*tail=NULL;
 
-    for(auto &it:mp){
-        if(it.second==1){
-            ListNode* temp=new ListNode(it.first);
+    // for(auto &it:mp){
+    //     if(it.second==1){
+    //         ListNode* temp=new ListNode(it.first);
 
 
-            if(!head2){
-                head2=temp;
-                tail=head2;
-            }
-            else{
-                tail->next=temp;
-                tail=tail->next;
-            }
-        }
+    //         if(!head2){
+    //             head2=temp;
+    //             tail=head2;
+    //         }
+    //         else{
+    //             tail->next=temp;
+    //             tail=tail->next;
+    //         }
+    //     }
        
-    }
+    // }
 
-      return head2;
+    //   return head2;
+
+      //tc o(n)
+      //sc o(n)
+
+      //better optimisied two ptr
+
+
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+      ListNode* dummy = new ListNode(0,head);
+      ListNode* prev=dummy;
+      ListNode* curr=head;
+
+      while(curr){
+        while(curr->next && curr->val==curr->next->val){
+            curr=curr->next;
+        }
+        
+
+        if(prev->next!=curr){
+            prev->next=curr->next;
+        }
+        else{
+            prev=prev->next;
+        }
+
+        curr=curr->next;
+      }
+
+      return dummy->next;
     }
 };
