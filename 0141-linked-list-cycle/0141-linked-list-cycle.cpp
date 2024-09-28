af3c8199a7d91bbf31f10a22f921dcd2e986bc77
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2024-09-28 15:08:01 +0530

    Time: 8 ms (71.37%), Space: 10.7 MB (97.13%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,84 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+//  if(head==NULL){
+//         return false;
+//     }
+//     ListNode* temp=head;
+//     map<ListNode*,bool>visited;
+//     while(temp!=NULL){
+//         if (visited[temp] == true) {
+//           return true;
+//         }
+//         visited[temp] = true;
+//         temp = temp->next;
+//     }
+//     return false;
+//     }
+    //tc:o(n)
+    //sc:o(n)
+
+    //to optimise space complexity to o(1)-- use floyd cycle detection algo
+    
+
+//     if(head==NULL || head->next==NULL){
+//         return false;
+//     }
+
+//     ListNode* slow=head;
+//     ListNode* fast=head;
+//     while(slow!=NULL && fast!=NULL){
+//         fast=fast->next;
+//         if(fast==NULL){
+//             break;
+//         }
+//         fast=fast->next;
+//         slow=slow->next;
+
+//         if(fast==slow){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+
+if(head==NULL || head->next==NULL){
+    return false;
+}
+
+ListNode* slow=head;
+ListNode* fast=head;
+
+while(slow!=NULL && fast!=NULL){
+    fast=fast->next;
+    if(fast==NULL){
+        break;
+    }
+    fast=fast->next;
+    slow=slow->next;
+
+    if(slow==fast){
+        return true;
+        
+    }
+    
+}
+
+return false;
+    }
+
+//tc:o(n)
+//sc:o(1)
+
+};
