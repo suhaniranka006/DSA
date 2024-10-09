592fd7b1de816c602180cb6316030de88190445e
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2024-10-09 22:42:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (9.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,21 +2,44 @@ class Solution {
 public:
     int climbStairs(int n) {
 
+        //best approach
+        // if(n<=1){
+        //     return 1;
+        // }
+        // int prev1=1;
+        // int prev2=1;
+        // int curr;
+
+        // for(int i=2;i<=n;i++){
+        //     curr=prev1+prev2;
+        //     prev2=prev1;
+        //     prev1=curr;
+        // }
+
+        // return curr;
+        //tc o(n)
+        //sc o(1)
+
+
+        //by memoization
+
+
+
+      vector<int>dp(n+1,-1);
+     return  memo(n,dp);
         
+    }
+
+
+    int memo(int n,vector<int>&dp){
         if(n<=1){
             return 1;
         }
-        int prev1=1;
-        int prev2=1;
-        int curr;
 
-        for(int i=2;i<=n;i++){
-            curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
+        if(dp[n]!=-1){
+            return dp[n];
         }
 
-        return curr;
-        
+        return dp[n]=memo(n-1,dp)+memo(n-2,dp);
     }
 };
