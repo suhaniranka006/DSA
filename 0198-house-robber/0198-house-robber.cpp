d9a436d176716650b43020366fd16ecd25012ffe
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2024-10-12 13:15:45 +0530

    Time: 0 ms (100%), Space: 9.7 MB (11.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -29,41 +29,73 @@ public:
     // }
 
 
-    //tc o(2^n)
+    //tc o(2^n) *k
     //sc o(n)
 
     //as in this problem we will find overlapping recursion subproblems so we can use memoization
     //as f(1) will hit everytime
 
+
+
     //memoization
-    //tc o(n)^k
+    //tc o(n)
     //sc o(n)+o(n)
+
+    // int rob(vector<int>&nums){
+    //     int n=nums.size();
+    //     vector<int>dp(n,-1);
+    //     return f(0,nums,dp);
+        
+    // }
+
+    // int f(int ind,vector<int>&nums,vector<int>&dp){
+    //    if(ind>=nums.size()){
+    //     return 0;
+    //    }
+    //     if(dp[ind]!=-1){
+    //         return dp[ind];
+    //     }
+
+    //     int pick=nums[ind]+f(ind+2,nums,dp);
+    //     int nonPick=f(ind+1,nums,dp);
+
+    //     return dp[ind]=max(pick,nonPick);
+
+
+    // }
+
+
+
+//tabulation
+//tc o(n)
+//sc o(n)
+
 
     int rob(vector<int>&nums){
         int n=nums.size();
         vector<int>dp(n,-1);
-        return f(0,nums,dp);
-        
+        return f(n-1,nums,dp);
     }
 
     int f(int ind,vector<int>&nums,vector<int>&dp){
-       if(ind>=nums.size()){
-        return 0;
-       }
+        dp[0]=nums[0];
+        int neg=0;
 
-       
+        for(int i=1;i<nums.size();i++){
+            int take=nums[i];
+            if(i>1){
+                take+=dp[i-2];
+            }
+            int nonTake=0+dp[i-1];
 
-        if(dp[ind]!=-1){
-            return dp[ind];
+            dp[i]=max(take,nonTake);
+
         }
 
-        int pick=nums[ind]+f(ind+2,nums,dp);
-        int nonPick=f(ind+1,nums,dp);
-
-        return dp[ind]=max(pick,nonPick);
-
-
+        return dp[ind];
     }
+
+
 
 
 
