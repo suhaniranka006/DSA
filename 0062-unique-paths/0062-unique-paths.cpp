739ed7cab56ee3b85522adbd82740a0798bcb557
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2024-10-14 18:01:57 +0530

    Time: 4 ms (13.66%), Space: 8 MB (51.84%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -27,35 +27,70 @@ public:
 
         //tc o(n*m)
         //sc o(m-1)+o(n-1) + o(n*m)
-      int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return f(m-1,n-1,dp);
-      }
+    //   int uniquePaths(int m, int n) {
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //     return f(m-1,n-1,dp);
+    //   }
 
-      int f(int i,int j,vector<vector<int>>&dp){
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
+    //   int f(int i,int j,vector<vector<int>>&dp){
+    //     if(i==0 && j==0){
+    //         return 1;
+    //     }
+    //     if(i<0 || j<0){
+    //         return 0;
+    //     }
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
        
-        int left=f(i,j-1,dp);
-        int up=f(i-1,j,dp);
-        return dp[i][j]=left+up;
-      }
+    //     int left=f(i,j-1,dp);
+    //     int up=f(i-1,j,dp);
+    //     return dp[i][j]=left+up;
+    //   }
 
 
       //tabulation
       //tc o(n*m)
       //sc o(n*m)
         
-        //  int uniquePaths(int m, int n) {
+         int uniquePaths(int m, int n) {
 
-        //  }
+            vector<vector<int>>dp(m,vector<int>(n,0));
+
+            return f(m,n,dp);
+
+         }
+
+         int f(int m,int n,vector<vector<int>>&dp){
+
+            //base case
+          
+
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+
+                    if(i==0 && j==0){
+                    dp[i][j]=1;
+                    }
+                    else{
+
+                          int up=0;
+            int left=0;
+                        if(j>0){
+                            left=dp[i][j-1];
+                        }
+
+                        if(i>0){
+                            up=dp[i-1][j];
+                        }
+                        dp[i][j]=up+left;
+                    }
+
+                }
+            }
+
+            return dp[m-1][n-1];
+         }
 
 };
