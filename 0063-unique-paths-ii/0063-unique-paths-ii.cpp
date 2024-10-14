4bc9ca3979b047bb65ba14771ce0814830c7b7b7
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2024-10-14 19:24:04 +0530

    Time: 0 ms (100%), Space: 10.6 MB (6.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
 
         //by rectursion
-    // int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        //tc o(2^m*n)
+        //sc o(m*n)
 
+    // int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
     //     //quite similar to previous one with slight changes
 
 
@@ -37,39 +39,86 @@ public:
 
 
     //memoization
-     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    //tc o(m*n)
+    //sc o(m*n)+o(m*n)
+    //  int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
-        //quite similar to previous one with slight changes
+    //     //quite similar to previous one with slight changes
 
 
-         int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
-            vector<vector<int>>dp(m,vector<int>(n,-1));
-        if(obstacleGrid[0][0]==1){
-            return 0;
-        }
-        return f(m-1,n-1,obstacleGrid,dp);
+    //      int m=obstacleGrid.size();
+    //     int n=obstacleGrid[0].size();
+    //         vector<vector<int>>dp(m,vector<int>(n,-1));
+    //     if(obstacleGrid[0][0]==1){
+    //         return 0;
+    //     }
+    //     return f(m-1,n-1,obstacleGrid,dp);
         
-    }
+    // }
 
 
-    int f(int i,int j,vector<vector<int>>&obstacleGrid,vector<vector<int>>&dp){
+    // int f(int i,int j,vector<vector<int>>&obstacleGrid,vector<vector<int>>&dp){
 
        
-        if(i<0 || j<0 || obstacleGrid[i][j]==1){
-            return 0;
-        }
+    //     if(i<0 || j<0 || obstacleGrid[i][j]==1){
+    //         return 0;
+    //     }
 
-        if(i==0 && j==0){
-            return 1;
-        }
+    //     if(i==0 && j==0){
+    //         return 1;
+    //     }
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int left=f(i,j-1,obstacleGrid,dp);
-        int up=f(i-1,j,obstacleGrid,dp);
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int left=f(i,j-1,obstacleGrid,dp);
+    //     int up=f(i-1,j,obstacleGrid,dp);
 
-        return dp[i][j]=left+up;
+    //     return dp[i][j]=left+up;
+    // }
+
+
+    //tabulation
+    //tc o(m*n)
+    //sc o(m*n)
+
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+            int m=obstacleGrid.size();
+            int n=obstacleGrid[0].size();
+            vector<vector<int>>dp(m,vector<int>(n,-1));
+
+          
+
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(obstacleGrid[i][j]==1){
+                        dp[i][j]=0;
+                    }
+
+
+                    else if(i==0 && j==0){
+                        dp[i][j]=1;
+                    }
+                    else {
+                        int up=0,left=0;
+
+                        if(i>0){
+                        up=dp[i-1][j];
+                        }
+
+                        if(j>0){
+                        left=dp[i][j-1];
+                        }
+
+                        dp[i][j]=up+left;
+
+
+                    }
+                }
+            }
+            return dp[m-1][n-1];
     }
+
+
 };
