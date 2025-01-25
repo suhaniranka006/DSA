73c419f2f17457c7cda0042e9b8841ec941ec68c
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-01-25 06:12:43 +0530

    Time: 0 ms (100%), Space: 10.7 MB (49.13%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -35,21 +35,58 @@ class Solution {
 
 //better approach
 public:
-    vector<vector<int>>v2;
-    void per(vector<int>v,int l,int h){
-        if(l==h){
-            v2.push_back(v);
+    // vector<vector<int>>v2;
+    // void per(vector<int>v,int l,int h){
+    //     if(l==h){
+    //         v2.push_back(v);
+    //     }
+    //     else{
+    //         for(int i=l;i<h;i++){
+    //             swap(v[l],v[i]);
+    //             per(v,l+1,h);
+    //             swap(v[l],v[i]);
+    //         }
+    //     }
+    // }
+
+
+    void solve(vector<int>&nums,vector<int>&ds,int freq[],vector<vector<int>>&ans){
+        //base case
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return;
         }
-        else{
-            for(int i=l;i<h;i++){
-                swap(v[l],v[i]);
-                per(v,l+1,h);
-                swap(v[l],v[i]);
+
+        //iterate
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){  // check if item is already in ds
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                solve(nums,ds,freq,ans);
+                freq[i]=0;
+                ds.pop_back();
+
             }
         }
+
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        per(nums,0,nums.size());
-        return v2;
+        // per(nums,0,nums.size());
+        // return v2;
+
+
+        //brute extra space
+        vector<vector<int>>ans;
+        vector<int>ds;
+        int n=nums.size();
+        int freq[n];
+
+        for(int i=0;i<nums.size();i++){
+            freq[i]=0;
+        }
+            solve(nums,ds,freq,ans);
+            return ans;
+
+        
     }
 };
