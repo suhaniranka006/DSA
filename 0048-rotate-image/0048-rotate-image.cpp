64792b934c9d9d231cfe6e2b9fdf639d634fb046
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-01-26 00:51:31 +0530

    Time: 0 ms (100%), Space: 10.3 MB (33.94%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,30 +4,50 @@ public:
         //brute
         //rc o(n^2)
         //sc o(n^2)  
+        // int n=matrix.size();
+        // vector<vector<int>>temp(n,vector<int>(n,0));  
+        // solve(matrix,temp);
+
+
+        // //copy elemnts to original array
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         matrix[i][j]=temp[i][j];
+        //     }
+        // }
+
+
+        //optimal
+        //trnspose
         int n=matrix.size();
-        vector<vector<int>>temp(n,vector<int>(n,0));  
-        solve(matrix,temp);
-
-
-        //copy elemnts to original array
-
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=temp[i][j];
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
+        }
+
+        //reverse row
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 
-    void solve(vector<vector<int>>&matrix,vector<vector<int>>&temp){
-        int n=matrix.size();
+    //tc o(n*n)
+    //sc o(1)
 
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                temp[j][n-i-1]=matrix[i][j];
-
-            }
-        }
     
-    }
+    // void solve(vector<vector<int>>&matrix,vector<vector<int>>&temp){
+    //     int n=matrix.size();
+
+
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<n;j++){
+    //             temp[j][n-i-1]=matrix[i][j];
+
+    //         }
+    //     }
+    
+    // }
 };
