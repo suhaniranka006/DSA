2f2bc3c9b2bd89dd6833c3d82e682df579421107
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-01-25 06:43:09 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.07%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -12,7 +12,84 @@ public:
         //better for cpp usrs
         //we have inbuilt next_permutation stl
 
-        next_permutation(nums.begin(),nums.end());
+        //next_permutation(nums.begin(),nums.end()); // by stl
+
+    //     //best
+    //     //apply stl 
+    //     //logest prefix match
+    //     //find >i but smallest one
+    //     //try to place remaining in sorted way
+
+    //     //find break point
+    //     int ind=-1;
+    //     int n=nums.size();
+    //     for(int i=n-2;i>=0;i--){
+    //         if(nums[i]>nums[i+1]){
+    //             ind=i; //break pt
+    //             break;
+    //         }
+    //     }
+
+    //     //if break pt doent exist
+    //     if(ind==-1){
+    //         reverse(nums.begin(),nums.end());
+            
+    //     }
+    //     //find next greater eleemnt
+    //     for(int i=n-1;i>=0;i--){
+    //         if(nums[i]>nums[ind]){
+    //             swap(nums[i],nums[ind]);
+    //             break;
+    //         }
+    //     }
+
+    //     //reverse remaining
+    //     reverse(nums.begin()+ind+1,nums.end());
+        
+
+        
+    // }
+
+    solve(nums);
+    }
+
+
+   void solve(vector<int>&nums){
+         //best
+        //apply stl 
+        //logest prefix match
+        //find >i but smallest one
+        //try to place remaining in sorted way
+
+        //find break point
+        int ind=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i; //break pt
+                break;
+            }
+        }
+
+        //if break pt doent exist
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return ;
+            
+        }
+        //find next greater eleemnt
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+
+        //reverse remaining
+        reverse(nums.begin()+ind+1,nums.end());
+        
+
         
     }
+    
 };
