518438d8fa6a6d6a73ecbff53e88838a614a3468
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-12 00:29:09 +0530

    Time: 3 ms (27.82%), Space: 17.2 MB (43.77%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,26 +12,62 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (!root) return ans;
+        // vector<vector<int>> ans;
+        // if (!root) return ans;
 
-        queue<TreeNode*> q;
+        // queue<TreeNode*> q;
+        // q.push(root);
+
+        // while (!q.empty()) {
+        //     int levelSize = q.size();
+        //     vector<int> currentLevel;
+
+        //     for (int i = 0; i < levelSize; ++i) {
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         currentLevel.push_back(node->val);
+
+        //         if (node->left) q.push(node->left);
+        //         if (node->right) q.push(node->right);
+        //     }
+
+        //     ans.push_back(currentLevel);
+        // }
+
+        // return ans;
+
+
+
+        vector<vector<int>>ans;
+
+        if(!root){
+            return ans;
+        }
+
+        queue<TreeNode*>q;
         q.push(root);
 
-        while (!q.empty()) {
-            int levelSize = q.size();
-            vector<int> currentLevel;
+        while(!q.empty()){
+            int levelsize=q.size();
+                vector<int>currentlevel;
+            for(int i=0;i<levelsize;i++){
 
-            for (int i = 0; i < levelSize; ++i) {
-                TreeNode* node = q.front();
-                q.pop();
-                currentLevel.push_back(node->val);
+            TreeNode* node=q.front();
+            q.pop();
+                currentlevel.push_back(node->val);
 
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                if(node->left){
+                    q.push(node->left);
+                }
+
+                if(node->right){
+                    q.push(node->right);
+                }
+
+
             }
+            ans.push_back(currentlevel);
 
-            ans.push_back(currentLevel);
         }
 
         return ans;
