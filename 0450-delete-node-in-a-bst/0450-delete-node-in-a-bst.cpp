4278e16ad3de21b9eefe5bca0432584d7b5796c6
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-14 20:14:23 +0530

    Time: 0 ms (100%), Space: 34.3 MB (63.05%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -13,7 +13,6 @@ class Solution {
 public:
 
 
-
     int successor(TreeNode* root){
         if(root==NULL){
             return -1;
@@ -21,34 +20,39 @@ public:
 
         if(root->left==NULL){
             return root->val;
-        }
-        return successor(root->left);
 
+        }
+
+        return successor(root->left);
     }
-       
     TreeNode* deleteNode(TreeNode* root, int key) {
-    
         if(root==NULL){
             return root;
         }
-      if(key>root->val){
+
+
+    if(key>root->val){
         root->right=deleteNode(root->right,key);
-      }
-      else if(key<root->val){
+    }
+    else if(key<root->val){
         root->left=deleteNode(root->left,key);
-      }
-      else{
+    }
+    else {
         if(root->left==NULL){
             return root->right;
         }
+
         if(root->right==NULL){
             return root->left;
         }
         root->val=successor(root->right);
         root->right=deleteNode(root->right,root->val);
-      }
-      return root;
+
         
+    }
+
+    return root;
+
         
     }
 };
