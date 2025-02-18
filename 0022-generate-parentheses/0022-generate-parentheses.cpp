7c102e381addff3f55488967ede2644544d4eff1
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-18 22:49:33 +0530

    Time: 2 ms (81.23%), Space: 13.3 MB (77.23%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,51 +1,114 @@
 class Solution {
 public:
 
-        vector<string>result;
+        // vector<string>result;
 
 
-        bool isValid(string curr){
-            int cnt=0;
+    //     bool isValid(string curr){
+    //         int cnt=0;
 
-            for(char ch:curr){
-                if(ch== '('){
-                    cnt++;
-                }
-                else{
-                    cnt--;
-                }
+    //         for(char ch:curr){
+    //             if(ch== '('){
+    //                 cnt++;
+    //             }
+    //             else{
+    //                 cnt--;
+    //             }
 
-                if(cnt<0){
-                    return false;
-                }
-            }
+    //             if(cnt<0){
+    //                 return false;
+    //             }
+    //         }
     
 
-            return cnt==0;
-        }
+    //         return cnt==0;
+    //     }
 
-        void solve(string &curr,int n){
+    //     void solve(string &curr,int n){
+    //         //base case
+    //         if(curr.length()==2*n){
+    //             if(isValid(curr)){
+    //                 result.push_back(curr);
+    //             }
+                
+    //                 return;
+                
+    //         }
+
+
+    //         curr.push_back('(');
+    //         solve(curr,n);
+    //         curr.pop_back();
+    //         curr.push_back(')');
+    //         solve(curr,n);
+    //         curr.pop_back();
+    //     }
+    // vector<string> generateParenthesis(int n) {
+    //     string curr="";
+    //     solve(curr,n);
+    //     return result;
+        
+    // }
+
+    //tc o(2^2n*2n)
+    //sc o(2n)
+
+
+    //better
+
+      //     vector<string>result;
+
+
+    //     bool isValid(string curr){
+    //         int cnt=0;
+
+    //         for(char ch:curr){
+    //             if(ch== '('){
+    //                 cnt++;
+    //             }
+    //             else{
+    //                 cnt--;
+    //             }
+
+    //             if(cnt<0){
+    //                 return false;
+    //             }
+    //         }
+    
+
+    //         return cnt==0;
+    //     }
+
+
+      vector<string>result;
+
+        void solve(string &curr,int n,int open,int close){
             //base case
             if(curr.length()==2*n){
-                if(isValid(curr)){
-                    result.push_back(curr);
-                }
+                // if(isValid(curr)){
+                   result.push_back(curr);
+                // }
                 
                     return;
                 
             }
 
-
+        
+        if(open<n){
             curr.push_back('(');
-            solve(curr,n);
+            solve(curr,n,open+1,close);
             curr.pop_back();
+        }
+
+            if(close<open){
             curr.push_back(')');
-            solve(curr,n);
+            solve(curr,n,open,close+1);
             curr.pop_back();
+            }
         }
     vector<string> generateParenthesis(int n) {
         string curr="";
-        solve(curr,n);
+        solve(curr,n,0,0);
         return result;
         
     }
