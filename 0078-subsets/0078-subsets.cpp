8749e652d750f2a35084dad48dea3c50d7262745
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-18 23:09:04 +0530

    Time: 0 ms (100%), Space: 12.6 MB (33.84%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,58 @@
 class Solution {
-    private:
-    void solve(vector<int>nums,vector<int>output,int index,vector<vector<int>>& ans){
-        //base case
-        if(index>=nums.size()){
-            ans.push_back(output);
-            return;
+public:
+
+
+        void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&output,int index){
+                if(index>=nums.size()){
+                    ans.push_back(output);
+                    return;
+                }
+
+                //exclude
+                solve(nums,ans,output,index+1);
+
+                //include
+                int element=nums[index];
+                output.push_back(element);
+                solve(nums,ans,output,index+1);
+                output.pop_back();
         }
 
-        //exclude
-        solve(nums,output,index+1,ans);
 
-        //include
-        int element=nums[index];
-        output.push_back(element);
-        solve(nums,output,index+1,ans);
-    }
-public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>output;
         int index=0;
-        solve(nums,output,index,ans);
+        solve(nums,ans,output,index);
         return ans;
     }
 };
+
+
+
+// class Solution {
+//     private:
+//     void solve(vector<int>nums,vector<int>output,int index,vector<vector<int>>& ans){
+//         //base case
+//         if(index>=nums.size()){
+//             ans.push_back(output);
+//             return;
+//         }
+
+//         //exclude
+//         solve(nums,output,index+1,ans);
+
+//         //include
+//         int element=nums[index];
+//         output.push_back(element);
+//         solve(nums,output,index+1,ans);
+//     }
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>output;
+//         int index=0;
+//         solve(nums,output,index,ans);
+//         return ans;
+//     }
+// };
