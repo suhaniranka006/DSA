852d2f678b066e9c0a34e044b3edc42cf5d39287
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-20 18:09:59 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.88%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,58 +1,44 @@
 class Solution {
-    private:
-    // void solve(string digit,string output,int index,vector<string>&ans,string mapping[]){
-    //     //base case
-    //     if(index>=digit.length()){
-    //         ans.push_back(output);
-    //         return;
-    //     }
-    //     int number = digit[index]-'0';
-    //     string value=mapping[number];
-
-    //     for(int i=0;i<value.length();i++){
-    //         output.push_back(value[i]);
-    //         solve(digit,output,index+1,ans,mapping);
-    //         output.pop_back();
-    //     }
-    // }
 public:
 
 
-    void solve(string digit,string output,int index,vector<string>&ans,string mapping[]){
-        if(index>=digit.length()){
+  void solve(int ind,string digits,vector<string>&ans,string output,string mapping[]){
+        if(ind>=digits.length()){
             ans.push_back(output);
             return;
         }
-        int number=digit[index]-'0';
+
+        int number=digits[ind]-'0';
         string value=mapping[number];
+
+
 
         for(int i=0;i<value.length();i++){
             output.push_back(value[i]);
-            solve(digit,output,index+1,ans,mapping);
+
+            solve(ind+1,digits,ans,output,mapping);
             output.pop_back();
         }
+
+        
     }
     vector<string> letterCombinations(string digits) {
-        // vector<string>ans;
-        // if(digits.length()==0){
-        //     return ans;
-        // }
-        // string output="";
-        // int index=0;
-        // string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
-        // solve(digits,output,index,ans,mapping);
-        // return ans;
 
+
+            string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
         vector<string>ans;
         if(digits.length()==0){
             return ans;
         }
         string output="";
-        int index=0;
-        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(digits,output,index,ans,mapping);
+        int ind=0;
+        
+        solve(ind,digits,ans,output,mapping);
         return ans;
+        
     }
+
+  
 };
