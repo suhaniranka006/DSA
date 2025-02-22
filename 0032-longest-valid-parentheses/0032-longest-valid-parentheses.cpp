b5a5064e70e71ae75869cb85ee837c50b9f56e93
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-22 22:37:12 +0530

    Time: 0 ms (100%), Space: 9.7 MB (87.11%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,30 +1,75 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-      stack<int>st;
+    //   stack<int>st;
 
-      st.push(-1);
-      int len=0;
-      int curr=0;
+    //   st.push(-1);
+      
+    //   int curr=0;
 
-      for(int i=0;i<s.length();i++){
+    //   for(int i=0;i<s.length();i++){
+    //     char ch=s[i];
+    //     if(ch=='('){
+    //         st.push(i);
+    //     }
+    //     else{
+    //         st.pop();
+    //         if(st.empty()){
+    //             st.push(i);
+
+    //         }
+    //         else{
+    //             int temp=st.top();
+    //             curr=max(curr,i-temp);
+    //         }
+    //     }
+    //   }
+    //   return curr;
+    //tc o(n)
+    //sc o(n)
+
+
+    //better 
+    //two pass
+
+    
+    int len=0;
+    int left=0;
+    int right=0;
+    for(int i=0;i<s.length();i++){
         char ch=s[i];
         if(ch=='('){
-            st.push(i);
+            left++;
         }
         else{
-            st.pop();
-            if(st.empty()){
-                st.push(i);
-
-            }
-            else{
-                int temp=st.top();
-                curr=max(curr,i-temp);
-            }
+            right++;
         }
-      }
-      return curr;
+        if(left==right){
+            len=max(len,2*left);
+        }
+        else if(right>left){  //unbalanced
+        left=right=0;
 
+        } 
+    }
+
+    left=right=0;
+
+    for(int i=s.length()-1;i>=0;i--){
+        char ch=s[i];
+        if(ch==')'){
+            right++;
+        }
+        else{
+        left++;
+        }
+        if(left==right){
+            len=max(len,2*left);
+        }
+        else if(left>right){
+            left=right=0;
+        }
+    }
+    return len;
     }
 };
