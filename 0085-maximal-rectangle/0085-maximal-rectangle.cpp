7ddce628e781e0c488169430941101c1c5f2c961
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-25 22:13:49 +0530

    Time: 4 ms (78.87%), Space: 17.8 MB (75.97%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,36 +1,86 @@
 class Solution {
 public:
+
+  int histogram(vector<int>&heights){
+    int n=heights.size();
+    stack<int>st;
+  int max_area=0;
+
+  for(int i=0;i<=n;i++){
+      int h = (i == n) ? 0 : heights[i];
+    while(!st.empty()  && h < heights[st.top()]){
+      int height=heights[st.top()];
+      st.pop();
+      int width=st.empty() ? i: (i-st.top()-1);
+      int area=height*width;
+      max_area=max(max_area,area);
+    }
+    st.push(i);
+  
+  }
+  return max_area;
+  }
+
     int maximalRectangle(vector<vector<char>>& matrix) {
+        // int rows=matrix.size();
+        // if(rows==0){
+        //   return 0;
+        // }
+        // int cols=matrix[0].size();
+        // int max_area=0;
+        // for(int i=0;i<rows;i++){
+        //   for(int j=0;j<cols;j++){
+
+        //     if(matrix[i][j]=='1'){
+        //     int min_width=INT_MAX;
+        //     for(int k=i;k<rows;k++){
+        //       if(matrix[k][j]=='0'){
+        //         break;
+        //       }
+        //       int width=0;
+
+        //       while(j+width<cols && matrix[k][j+width]=='1'){
+        //         width++;
+        //       }
+        //       min_width=min(min_width,width);
+        //       int area=(k-i+1 )*min_width;
+        //       max_area=max(max_area,area);
+        //     }
+
+
+        //   }
+        // }
+        // }
+        // return max_area;
+
+        //tc o(rows,cols)^2
+        //sc o(1)
+
+
         int rows=matrix.size();
         if(rows==0){
           return 0;
         }
-        int cols=matrix[0].size();
-        int max_area=0;
-        for(int i=0;i<rows;i++){
-          for(int j=0;j<cols;j++){
+          int cols=matrix[0].size();
+          int max_area=0;
+          vector<int>heights(cols,0);
+        
 
-            if(matrix[i][j]=='1'){
-            int min_width=INT_MAX;
-            for(int k=i;k<rows;k++){
-              if(matrix[k][j]=='0'){
-                break;
+          for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+              if(matrix[i][j]=='1'){
+                heights[j]++;
               }
-              int width=0;
-
-              while(j+width<cols && matrix[k][j+width]=='1'){
-                width++;
+              else{
+                heights[j]=0;
               }
-              min_width=min(min_width,width);
-              int area=(k-i+1)*min_width;
-              max_area=max(max_area,area);
             }
-
-
+            int area=histogram(heights);
+            max_area=max(max_area,area);
           }
+        
+          return max_area;
         }
-        }
-        return max_area;
-    }
+    
 
 };
