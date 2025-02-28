73633c816410a82cdad9f9508d8e02b6f167f6a0
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-02-28 21:32:02 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -61,40 +61,88 @@ public:
   //   return -1;
 
 
-    int n=nums.size();
-    int st=0;
-    int end=n-1;
+    // int n=nums.size();
+    // int st=0;
+    // int end=n-1;
 
-    //find pivot 
+  
+
+    // while(st<=end){
+    //   int mid=(st+end)/2;
+
+    //   if(nums[mid]==target){
+    //     return mid;
+    //   }
+      
+    //   if(nums[st]<=nums[mid]){
+    //     if(target>=nums[st]  && target<nums[mid]){
+    //       end=mid-1;
+    //     }
+    //     else{
+    //         st=mid+1;
+    //     }
+    //   }
+    //   else {
+    //     if(target>nums[mid] && target<=nums[end]){
+    //       st=mid+1;
+    //     }
+    //     else{
+    //       end=mid-1;
+    //     }
+
+    //   }
+    // }
+    // return  -1;
+
+
+  //find pivot 
     //find target index
 
-    while(st<=end){
-      int mid=(st+end)/2;
 
-      if(nums[mid]==target){
-        return mid;
-      }
-      
-      if(nums[st]<=nums[mid]){
-        if(target>=nums[st]  && target<nums[mid]){
-          end=mid-1;
-        }
-        else{
-            st=mid+1;
-        }
-      }
-      else {
-        if(target>nums[mid] && target<=nums[end]){
-          st=mid+1;
-        }
-        else{
-          end=mid-1;
-        }
+    int n = nums.size();
+    if (n == 0) return -1;
 
-      }
+    int left = 0, right = n - 1;
+
+    // Find the pivot
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
     }
-    return  -1;
 
+    int pivot = left;
+
+    // Determine the search range
+    if (pivot == 0) {
+        left = 0;
+        right = n - 1;
+    } else if (target >= nums[0]) {
+        left = 0;
+        right = pivot - 1;
+    } else {
+        left = pivot;
+        right = n - 1;
+    }
+
+    // Binary search in the appropriate half
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
 
     }
 };
