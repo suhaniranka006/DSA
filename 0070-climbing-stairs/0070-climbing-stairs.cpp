6e0c468589fead2ffed68d3a380af618d81657ad
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-03-23 13:29:13 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
-    int climbStairs(int n) {
+ //by recursion
+        //o(2^n)
+      // if (n == 0 || n == 1) {
+        ///    return 1;
+       // }
+        //return climbStairs(n-1) + climbStairs(n-2);
+
+
 
         //best approach
         // if(n<=1){
@@ -29,18 +36,20 @@ public:
 
 
     //by tabulation
-    vector<int>dp(n+1,-1);
+    //tc o(n)
+    //sc o(n)  fo vector only
+    // vector<int>dp(n+1,-1);
 
-    dp[0]=1;
-    dp[1]=1;
-    int i;
-    for( i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-    }
+    // dp[0]=1;
+    // dp[1]=1;
+    // int i;
+    // for( i=2;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
+    // }
 
-    return dp[n];
+    // return dp[n];
         
-    }
+    //}
 
 
     // int memo(int n,vector<int>&dp){
@@ -54,4 +63,30 @@ public:
 
     //     return dp[n]=memo(n-1,dp)+memo(n-2,dp);
     // }
+    int climbStairs(int n) {
+      //recursion 
+      // if(n<=1){
+      //   return 1;
+      // }
+
+      // return climbStairs(n-1)+climbStairs(n-2);
+      //tc o(2^n)
+      //sc o(n)
+
+      //space optimisation 
+      if(n<=1){
+        return 1;
+      }
+      int prev1=1;
+      int prev2=1;
+      int curr;
+      for(int i=2;i<=n;i++){
+        curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+
+      }
+      return curr;
+        
+    }
 };
