b34342b11d2e5ea3608ae6c033e6014dfadaf675
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-03-23 13:33:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -91,24 +91,33 @@ public:
       //sc o(1)
 
       //memo
-      vector<int>dp(n+1,-1);
+      // vector<int>dp(n+1,-1);
     
 
-      return solve(n,dp);
+      // return solve(n,dp);
+
+      //tabulation
+      vector<int>dp(n+1,-1);
+      dp[0]=1;
+      dp[1]=1;
+      for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+      }
+      return dp[n];
         
     }
 
-    int solve(int n,vector<int>&dp){
-      if(n<=1){
-        return 1;
-      }
+    // int solve(int n,vector<int>&dp){
+    //   if(n<=1){
+    //     return 1;
+    //   }
 
-      if(dp[n]!=-1){
-        return dp[n];
-      }
+    //   if(dp[n]!=-1){
+    //     return dp[n];
+    //   }
 
-      return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    //   return dp[n]=solve(n-1,dp)+solve(n-2,dp);
       
 
-    }
+    // }
 };
