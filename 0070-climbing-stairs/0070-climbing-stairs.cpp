bb73e29500d5630f335c8f719193548d12a3a130
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-03-23 13:32:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -74,19 +74,41 @@ public:
       //sc o(n)
 
       //space optimisation 
+      // if(n<=1){
+      //   return 1;
+      // }
+      // int prev1=1;
+      // int prev2=1;
+      // int curr;
+      // for(int i=2;i<=n;i++){
+      //   curr=prev1+prev2;
+      //   prev2=prev1;
+      //   prev1=curr;
+
+      // }
+      // return curr;
+      //tc o(n)
+      //sc o(1)
+
+      //memo
+      vector<int>dp(n+1,-1);
+    
+
+      return solve(n,dp);
+        
+    }
+
+    int solve(int n,vector<int>&dp){
       if(n<=1){
         return 1;
       }
-      int prev1=1;
-      int prev2=1;
-      int curr;
-      for(int i=2;i<=n;i++){
-        curr=prev1+prev2;
-        prev2=prev1;
-        prev1=curr;
 
+      if(dp[n]!=-1){
+        return dp[n];
       }
-      return curr;
-        
+
+      return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+      
+
     }
 };
