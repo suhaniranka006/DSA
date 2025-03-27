affe51ae2406e2b395e47eb51bc62580abd7c2f9
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-03-27 10:43:11 +0530

    Time: 0 ms (100%), Space: 9.5 MB (8.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
 
-    //recursion
+
+ //recursion
     //tc o(2^m*n)
     //sc o(m-1)+o(n-1) path length
     // int uniquePaths(int m, int n) {
     //     return f(m-1,n-1);     
     // }
+
+
+
 
     // int f(int i,int j){
     //     //base case
@@ -32,6 +36,7 @@ public:
     //     return f(m-1,n-1,dp);
     //   }
 
+
     //   int f(int i,int j,vector<vector<int>>&dp){
     //     if(i==0 && j==0){
     //         return 1;
@@ -54,43 +59,91 @@ public:
       //tc o(n*m)
       //sc o(n*m)
         
-         int uniquePaths(int m, int n) {
+        //  int uniquePaths(int m, int n) {
 
-            vector<vector<int>>dp(m,vector<int>(n,0));
+        //     vector<vector<int>>dp(m,vector<int>(n,0));
 
-            return f(m,n,dp);
+        //     return f(m,n,dp);
 
-         }
+        //  }
 
-         int f(int m,int n,vector<vector<int>>&dp){
+        //  int f(int m,int n,vector<vector<int>>&dp){
+        //     //base case
+        //     for(int i=0;i<m;i++){
+        //         for(int j=0;j<n;j++){
 
-            //base case
-          
+        //             if(i==0 && j==0){
+        //             dp[i][j]=1;
+        //             }
+        //             else{
+        //                   int up=0;
+        //                     int left=0;
+        //                 if(j>0){
+        //                     left=dp[i][j-1];
+        //                 }
 
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
+        //                 if(i>0){
+        //                     up=dp[i-1][j];
+        //                 }
+        //                 dp[i][j]=up+left;
+        //             }
 
-                    if(i==0 && j==0){
-                    dp[i][j]=1;
-                    }
-                    else{
+        //         }
+        //     }
 
-                          int up=0;
-            int left=0;
-                        if(j>0){
-                            left=dp[i][j-1];
-                        }
+        //     return dp[m-1][n-1];
+        //  }
 
-                        if(i>0){
-                            up=dp[i-1][j];
-                        }
-                        dp[i][j]=up+left;
-                    }
+         //if there we are using prev row and prev col we can do space optimzation 
+         //in interviews they dont ask but striver have done it if u want but right now i am not doing it
 
-                }
-            }
+    int uniquePaths(int m, int n) {
 
-            return dp[m-1][n-1];
-         }
+      vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return f(m-1,n-1,dp);
+    }
+
+    //recursion
+    //tc o^(m*n)
+    //sc o(m-1,n-1)
+    // int f(int i,int j){
+    //   //base case
+    //   if(i==0 && j==0){
+    //     return 1;
+    //   }
+
+    //   if(i<0 || j<0){
+    //     return 0;
+    //   }
+
+    //   int left=f(i,j-1);
+    //   int right=f(i-1,j);
+
+    //   return left+right;
+    // }
+
+
+    //memoization
+    int f(int i,int j,vector<vector<int>>&dp){
+      if(i==0 && j==0){
+        return 1;
+      }
+       if(i<0 || j<0){
+        return 0;
+       }
+
+
+       if(dp[i][j]!=-1){
+        return dp[i][j];
+       }
+
+       int left=f(i,j-1,dp);
+       int up=f(i-1,j,dp);
+
+       return dp[i][j]=left+up;
+    }
+
+
+
 
 };
