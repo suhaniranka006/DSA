38cab6e77d87e20f862b06b78eda5d35df775186
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-03-27 15:54:16 +0530

    Time: 8 ms (61.18%), Space: 11.9 MB (49.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
+
+    //tc o(n^3)
+    //sc o(1)
     int lengthOfLongestSubstring(string s) {
-        set<char>charSet;
+       // int maxlen=0;
+        //brute force
+        //phle hr substring check kro
+        //uski uniqueness check kro
+        //   int n=s.size();
+        // for(int i=0;i<n;i++){
+        //   for(int j=i;j<n;j++){
+        //     string substring=s.substr(i,j-i+1);
+        //       if(isunique(substring)){
+        //         maxlen=max(maxlen,j-i+1);
+        //       }
+        //   }
+        // }
+        // return maxlen;
+
+        //optimised
+
+        int maxlen=0;
+        unordered_map<char,int>mp;
+      
         int left=0;
-        int right=0;
-        int maxLength=0;
-        for(right=0;right<s.size();right++){
-          if(charSet.count(s[right])==0){
-            charSet.insert(s[right]);
-            maxLength=max(maxLength,right-left+1);
+        int n=s.size();
+
+        for(int right=0;right<n;right++){
+          char c=s[right];
+          if(mp.count(c)  && mp[c]>=left){
+            left=mp[c]+1;
+
           }
-          else{
-            while(charSet.count(s[right])){
-                charSet.erase(s[left]);
-                left++;
-            }
-            charSet.insert(s[right]);
-          }
+          mp[c]=right;
+          maxlen=max(maxlen,right-left+1);
         }
-        return maxLength;
-        
+
+  return maxlen;
     }
+
+
+
+    // bool isunique(string &substring){
+    //   unordered_set<char>seen;
+    //   for(char c:substring){
+    //       if(seen.count(c)){
+    //         return false;
+    //       }
+    //       seen.insert(c);
+    //   }
+    //   return true;
+    
 };
