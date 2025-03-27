dc6ca2686da11d96b497505aa4efb8c3f03cf8fd
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-03-27 10:55:26 +0530

    Time: 0 ms (100%), Space: 12 MB (6.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-        //by rectursion
+     //by rectursion
         //tc o(2^m*n)
         //sc o(m*n)
 
@@ -83,42 +83,83 @@ public:
     //sc o(m*n)
 
 
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-            int m=obstacleGrid.size();
-            int n=obstacleGrid[0].size();
-            vector<vector<int>>dp(m,vector<int>(n,-1));
+    // int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    //         int m=obstacleGrid.size();
+    //         int n=obstacleGrid[0].size();
+    //         vector<vector<int>>dp(m,vector<int>(n,-1));
 
           
 
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(obstacleGrid[i][j]==1){
-                        dp[i][j]=0;
-                    }
+    //         for(int i=0;i<m;i++){
+    //             for(int j=0;j<n;j++){
+    //                 if(obstacleGrid[i][j]==1){
+    //                     dp[i][j]=0;
+    //                 }
 
 
-                    else if(i==0 && j==0){
-                        dp[i][j]=1;
-                    }
-                    else {
-                        int up=0,left=0;
+    //                 else if(i==0 && j==0){
+    //                     dp[i][j]=1;
+    //                 }
+    //                 else {
+    //                     int up=0,left=0;
 
-                        if(i>0){
-                        up=dp[i-1][j];
-                        }
+    //                     if(i>0){
+    //                     up=dp[i-1][j];
+    //                     }
 
-                        if(j>0){
-                        left=dp[i][j-1];
-                        }
+    //                     if(j>0){
+    //                     left=dp[i][j-1];
+    //                     }
 
-                        dp[i][j]=up+left;
+    //                     dp[i][j]=up+left;
 
 
-                    }
-                }
-            }
-            return dp[m-1][n-1];
+    //                 }
+    //             }
+    //         }
+    //         return dp[m-1][n-1];
+    // }
+
+
+//recursion
+//tc o(o^m*n)
+//sc o^m*n
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n=obstacleGrid.size();
+        int m=obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1){
+          return 0;
+        }
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,obstacleGrid,dp);
     }
 
+    // int f(int i,int j,vector<vector<int>>&obstacleGrid){
+    //   if(i<0 || j<0 || obstacleGrid[i][j]==1){
+    //     return 0;
+    //   }
+    //   if(i==0 && j==0){
+    //     return 1;
+    //   }
+    //   int left=f(i,j-1,obstacleGrid);
+    //   int up=f(i-1,j,obstacleGrid);
+    //   return left+up;
+    // }
 
+
+
+      int f(int i,int j,vector<vector<int>>&obstacleGrid,vector<vector<int>>&dp){
+      if(i==0 && j==0){
+        return 1;
+      }
+      if(i<0 || j<0 || obstacleGrid[i][j]==1){
+        return 0;
+      }
+      if(dp[i][j]!=-1){
+        return dp[i][j];
+      }
+      int left=f(i,j-1,obstacleGrid,dp);
+      int up=f(i-1,j,obstacleGrid,dp);
+      return dp[i][j]= left+up;
+    }
 };
