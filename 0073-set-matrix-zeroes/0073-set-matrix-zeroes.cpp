21378f193644c403ef518f322fe32c95d31eb0e0
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-04-02 09:59:59 +0530

    Time: 0 ms (100%), Space: 19.1 MB (7.19%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+          int rows=matrix.size();
+          int cols=matrix[0].size();
+
+      vector<vector<int>>grid(rows,vector<int>(cols,-1));
+      f(matrix,grid);
+      
+
+         
+    }
+
+    void f(vector<vector<int>>&matrix,vector<vector<int>>&grid){
+      int rows=matrix.size();
+      int cols=matrix[0].size();
+
+
+           for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+        grid[i][j]=matrix[i][j];
+        }
+      }
+      for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+          if(matrix[i][j]==0){
+            for(int k=0;k<grid[0].size();k++){
+              grid[i][k]=0;
+            }
+            for(int l=0;l<grid.size();l++){
+              grid[l][j]=0;
+            }
+          }
+          else {
+
+            if(matrix[i][j]==1 && grid[i][j]!=0){
+            grid[i][j]=1;
+            }
+           
+          }
+        }
+      }
+
+      for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+          matrix[i][j]=grid[i][j];
+        }
+      }
+    
+    }
+};
