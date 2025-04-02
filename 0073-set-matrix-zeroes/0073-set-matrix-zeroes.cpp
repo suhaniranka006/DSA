2840d5c9c1ac017dca7b9b3dc11e456031c8d61a
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-04-02 17:14:07 +0530

    Time: 0 ms (100%), Space: 18.7 MB (53.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -51,34 +51,105 @@ public:
     // }
 
 
-      //optmised approach
+      //better approach
       //2 pass solution
-     void setZeroes(vector<vector<int>>& matrix) {
-          int rows=matrix.size();
-          int cols=matrix[0].size();
-          unordered_set<int>st1;  // row with 0
-          unordered_set<int>st2;  //col with 0
+      //tc o(m*n)
+      //sc o(m+n)
+    //  void setZeroes(vector<vector<int>>& matrix) {
+    //       int rows=matrix.size();
+    //       int cols=matrix[0].size();
+    //       unordered_set<int>st1;  // row with 0
+    //       unordered_set<int>st2;  //col with 0
 
-            //first pass
-          for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-              if(matrix[i][j]==0){
-                st1.insert(i);
-                st2.insert(j);
-              }
-            }
-          }
+    //         //first pass
+    //       for(int i=0;i<rows;i++){
+    //         for(int j=0;j<cols;j++){
+    //           if(matrix[i][j]==0){
+    //             st1.insert(i);
+    //             st2.insert(j);
+    //           }
+    //         }
+    //       }
 
-          //2nd pass
-          for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-              if(st1.count(i) || st2.count(j)){
-                matrix[i][j]=0;
-              }
-            }
-          }
+    //       //2nd pass
+    //       for(int i=0;i<rows;i++){
+    //         for(int j=0;j<cols;j++){
+    //           if(st1.count(i) || st2.count(j)){
+    //             matrix[i][j]=0;
+    //           }
+    //         }
+    //       }
 
         
+    //       }
+
+
+
+      //optimised approach
+      //tc o(m*n)
+      //sc o(m+n)
+      void setZeroes(vector<vector<int>>& matrix) {
+        //2 pass solution
+
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        
+
+        //use frist row or column as markers
+
+          bool firstrow=false;
+          bool firstcol=false;
+        for(int i=0;i<rows;i++){
+          if(matrix[i][0]==0){
+            firstrow=true;
+            break;
+
           }
+        }
+        for(int j=0;j<cols;j++){
+          if(matrix[0][j]==0){
+            firstcol=true;
+            break;
+          }
+        }
+
+
+        //mark 0s for first row and col
+        for(int i=1;i<rows;i++){
+          for(int j=1;j<cols;j++){
+            if(matrix[i][j]==0){
+              matrix[i][0]=0;
+              matrix[0][j]=0;
+            }
+          }
+        }
+
+        //mark zeroes for remaining
+        for(int i=1;i<rows;i++){
+          for(int j=1;j<cols;j++){
+          
+            if(matrix[i][0]==0 || matrix[0][j]==0){
+              matrix[i][j]=0;
+            }
+          }
+        }
+
+
+          //mark first row , col if needed
+          if(firstrow){
+            for(int i=0;i<rows;i++){
+              matrix[i][0]=0;
+            }
+          }
+
+          if(firstcol){
+            for(int i=0;i<cols;i++){
+              matrix[0][i]=0;
+            }
+          }
+       
+      
+
+      }
      
 };
