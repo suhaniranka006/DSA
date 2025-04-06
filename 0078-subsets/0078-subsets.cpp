f4e3b75b5405485800f07c053a3cd8d9aef1b485
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-04-06 11:28:14 +0530

    Time: 3 ms (33.1%), Space: 9.9 MB (81.47%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,77 +1,21 @@
 class Solution {
 public:
-
-
-        void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&output,int index){
-                if(index>=nums.size()){
-                    ans.push_back(output);
-                    return;
-                }
-
-                //exclude
-                solve(nums,ans,output,index+1);
-
-                //include
-                int element=nums[index];
-                output.push_back(element);
-                solve(nums,ans,output,index+1);
-                output.pop_back();
-        }
-
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        // vector<vector<int>>ans;
-        // vector<int>output;
-        // int index=0;
-        // solve(nums,ans,output,index);
-        // return ans;
-
+        int n=nums.size();
         vector<vector<int>>ans;
-       
-            int n=nums.size();
-        int subset=1<<n;
+        int subset=1<<n; //as 2^n subset
 
         for(int i=0;i<subset;i++){
-             vector<int>temp;
-            for(int j=0;j<n;j++){
-                if(i & (1<<j)){
-                    temp.push_back(nums[j]);
-                }
+          vector<int>temp;
+          for(int j=0;j<n;j++){
+            if(i & (1<<j))
+            {
+            temp.push_back(nums[j]);
             }
-            ans.push_back(temp);
+          }
+          ans.push_back(temp);
         }
         return ans;
     }
-
-    //tc o(2^n)
-    //sc o(2^n *n)
 };
 
-
-
-// class Solution {
-//     private:
-//     void solve(vector<int>nums,vector<int>output,int index,vector<vector<int>>& ans){
-//         //base case
-//         if(index>=nums.size()){
-//             ans.push_back(output);
-//             return;
-//         }
-
-//         //exclude
-//         solve(nums,output,index+1,ans);
-
-//         //include
-//         int element=nums[index];
-//         output.push_back(element);
-//         solve(nums,output,index+1,ans);
-//     }
-// public:
-//     vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<vector<int>>ans;
-//         vector<int>output;
-//         int index=0;
-//         solve(nums,output,index,ans);
-//         return ans;
-//     }
-// };
