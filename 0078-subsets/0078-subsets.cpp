a9474a7bb7ab8bea37fb0dcd2b3e071787d6daa7
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-04-06 11:40:52 +0530

    Time: 3 ms (33.1%), Space: 12.7 MB (32.54%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>ans;
-        int subset=1<<n; //as 2^n subset
+        // int n=nums.size();
+        // vector<vector<int>>ans;
+        // int subset=1<<n; //as 2^n subset
 
-        for(int i=0;i<subset;i++){
-          vector<int>temp;
-          for(int j=0;j<n;j++){
-            if(i & (1<<j))
-            {
-            temp.push_back(nums[j]);
-            }
-          }
-          ans.push_back(temp);
-        }
+        // for(int i=0;i<subset;i++){
+        //   vector<int>temp;
+        //   for(int j=0;j<n;j++){
+        //     if(i & (1<<j))
+        //     {
+        //     temp.push_back(nums[j]);
+        //     }
+        //   }
+        //   ans.push_back(temp);
+        // }
+        // return ans;
+
+
+        //recursive approach
+        vector<vector<int>>ans;
+        vector<int>temp;
+        f(0,nums,ans,temp);
         return ans;
+    }
+    //tc o(2^n)
+    //sc o(2^(n*n))
+
+    void f(int index,vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp){
+      //base case
+      if(index>=nums.size()){
+        ans.push_back(temp);
+        return;
+      }
+
+      //exclude
+      f(index+1,nums,ans,temp);
+      //include
+      int ele=nums[index];
+      temp.push_back(ele);
+
+        f(index+1,nums,ans,temp);
+        temp.pop_back();
     }
 };
 
