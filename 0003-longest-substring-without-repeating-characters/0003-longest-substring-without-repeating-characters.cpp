7bd9949343b1436f34c144ba232b6757e3f082c0
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-04-08 21:02:40 +0530

    Time: 24 ms (16.24%), Space: 14.2 MB (36.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,55 +1,60 @@
 class Solution {
 public:
 
-    //tc o(n^3)
-    //sc o(1)
+
+      // bool isunique(string &substring){
+      //   unordered_set<char>st;
+
+      //   for(char c:substring){
+      //     if(st.count(c)){
+      //       return false;
+      //     }
+      //     st.insert(c);
+      //   }
+      //   return true;
+      // }
+
+
     int lengthOfLongestSubstring(string s) {
-       // int maxlen=0;
-        //brute force
-        //phle hr substring check kro
-        //uski uniqueness check kro
-        //   int n=s.size();
+        
+        //brute force 
+        //find all substring
+        // //check unique characters
+        // int maxlen=0;
+        // int n=s.size();
         // for(int i=0;i<n;i++){
         //   for(int j=i;j<n;j++){
         //     string substring=s.substr(i,j-i+1);
         //       if(isunique(substring)){
         //         maxlen=max(maxlen,j-i+1);
         //       }
+        //     }
         //   }
-        // }
-        // return maxlen;
+        
+      //  return maxlen;
+        //tc o(n^3)
+        //sc o(n)
+        //gives tle 
+
 
         //optimised
-
-        int maxlen=0;
-        unordered_map<char,int>mp;
-      
+        //sliding window
         int left=0;
-        int n=s.size();
-
-        for(int right=0;right<n;right++){
-          char c=s[right];
-          if(mp.count(c)  && mp[c]>=left){
-            left=mp[c]+1;
-
+        int maxlen=0;
+        unordered_set<char>st;
+        for(int right=0;right<s.size();right++){
+          while(st.count(s[right])){
+            st.erase(s[left]);
+            left++;
           }
-          mp[c]=right;
+          st.insert(s[right]);
           maxlen=max(maxlen,right-left+1);
-        }
 
-  return maxlen;
+         
+          
+        }
+        return maxlen;
     }
 
-
-
-    // bool isunique(string &substring){
-    //   unordered_set<char>seen;
-    //   for(char c:substring){
-    //       if(seen.count(c)){
-    //         return false;
-    //       }
-    //       seen.insert(c);
-    //   }
-    //   return true;
-    
+   
 };
