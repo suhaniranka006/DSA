ecc65536006f77e335eb3006ae15bf8a156b8098
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-04-19 22:01:45 +0530

    Time: 0 ms (100%), Space: 11 MB (64.65%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,21 +1,52 @@
 class Solution {
 public:
+
+    //tc o(n)
+    //sc o(n)
     int numberOfSubstrings(string s) {
-      int cnt=0;
-      int left=0;
-      unordered_map<char,int>mp;
-      int n=s.size();
-      for(int right=0;right<s.size();right++){
-        mp[s[right]]++;
-        //contain all 3 char
-        while(mp['a']>0 && mp['b']>0 && mp['c']){
-        cnt+=n-right;
-        mp[s[left]]--;
-        left++;
-        }
+      // int cnt=0;
+      // int left=0;
+      // unordered_map<char,int>mp;
+      // int n=s.size();
+      // for(int right=0;right<s.size();right++){
+      //   mp[s[right]]++;
+      //   //contain all 3 char
+      //   while(mp['a']>0 && mp['b']>0 && mp['c']){
+      //   cnt+=n-right;
+      //   mp[s[left]]--;
+      //   left++;
+      //   }
+      // }
+
+    //  return cnt;
+
+
+    //optimied sliding window
+    //last occ
+
+    int lasta=-1;
+    int lastb=-1;
+    int lastc=-1;
+    int cnt=0;
+    int left=0;
+    for(int right=0;right<s.size();right++){
+      if(s[right]=='a'){
+        lasta=right;
+      }
+      else if(s[right]=='b'){
+        lastb=right;
+      }
+      else{
+        lastc=right;
       }
 
-      return cnt;
+
+      if(lasta!=-1 && lastb!=-1 && lastc!=-1){
+        cnt+=min({lasta,lastb,lastc})+1;
+      }
+    
+    }
+    return cnt;
 
         
     }
