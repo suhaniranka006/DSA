439b8a72939155dcd970bbdb84d42cc0b4c88c95
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-05-28 10:14:57 +0530

    Time: 0 ms (100%), Space: 19.2 MB (6.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,155 +1,40 @@
 class Solution {
 public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,1));
 
-      //brute approach
-    // void setZeroes(vector<vector<int>>& matrix) {
-    //       int rows=matrix.size();
-    //       int cols=matrix[0].size();
+        f(matrix,vis,n,m);
 
-    //   vector<vector<int>>grid(rows,vector<int>(cols,-1));
-    //   f(matrix,grid);     
-    // }
-
-    // void f(vector<vector<int>>&matrix,vector<vector<int>>&grid){
-    //   int rows=matrix.size();
-    //   int cols=matrix[0].size();
-
-
-    //        for(int i=0;i<rows;i++){
-    //     for(int j=0;j<cols;j++){
-    //     grid[i][j]=matrix[i][j];
-    //     }
-    //   }
-    //   for(int i=0;i<rows;i++){
-    //     for(int j=0;j<cols;j++){
-    //       if(matrix[i][j]==0){
-    //         for(int k=0;k<grid[0].size();k++){
-    //           grid[i][k]=0;
-    //         }
-    //         for(int l=0;l<grid.size();l++){
-    //           grid[l][j]=0;
-    //         }
-    //       }
-    //       // else {
-
-    //       //   if(matrix[i][j]==1 && grid[i][j]!=0){
-    //       //   grid[i][j]=1;
-    //       //   }
-           
-    //       // }
-    //     }
-    //   }
-
-    //   for(int i=0;i<rows;i++){
-    //     for(int j=0;j<cols;j++){
-    //       matrix[i][j]=grid[i][j];
-    //     }
-    //   }
-    //   // otc  (m*n(m+n))
-    //   //sc o(m*n)
-
-    // }
-
-
-      //better approach
-      //2 pass solution
-      //tc o(m*n)
-      //sc o(m+n)
-    //  void setZeroes(vector<vector<int>>& matrix) {
-    //       int rows=matrix.size();
-    //       int cols=matrix[0].size();
-    //       unordered_set<int>st1;  // row with 0
-    //       unordered_set<int>st2;  //col with 0
-
-    //         //first pass
-    //       for(int i=0;i<rows;i++){
-    //         for(int j=0;j<cols;j++){
-    //           if(matrix[i][j]==0){
-    //             st1.insert(i);
-    //             st2.insert(j);
-    //           }
-    //         }
-    //       }
-
-    //       //2nd pass
-    //       for(int i=0;i<rows;i++){
-    //         for(int j=0;j<cols;j++){
-    //           if(st1.count(i) || st2.count(j)){
-    //             matrix[i][j]=0;
-    //           }
-    //         }
-    //       }
-
-        
-    //       }
-
-
-
-      //optimised approach
-      //tc o(m*n)
-      //sc o(m+n)
-      void setZeroes(vector<vector<int>>& matrix) {
-        //2 pass solution
-
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        
-
-        //use frist row or column as markers
-
-          bool firstrow=false;
-          bool firstcol=false;
-        for(int i=0;i<rows;i++){
-          if(matrix[i][0]==0){
-            firstrow=true;
-            break;
-
-          }
-        }
-        for(int j=0;j<cols;j++){
-          if(matrix[0][j]==0){
-            firstcol=true;
-            break;
-          }
-        }
-
-
-        //mark 0s for first row and col
-        for(int i=1;i<rows;i++){
-          for(int j=1;j<cols;j++){
-            if(matrix[i][j]==0){
-              matrix[i][0]=0;
-              matrix[0][j]=0;
-            }
-          }
-        }
-
-        //mark zeroes for remaining
-        for(int i=1;i<rows;i++){
-          for(int j=1;j<cols;j++){
-          
-            if(matrix[i][0]==0 || matrix[0][j]==0){
-              matrix[i][j]=0;
-            }
-          }
-        }
-
-
-          //mark first row , col if needed
-          if(firstrow){
-            for(int i=0;i<rows;i++){
-              matrix[i][0]=0;
-            }
-          }
-
-          if(firstcol){
-            for(int i=0;i<cols;i++){
-              matrix[0][i]=0;
-            }
-          }
-       
       
+    }
 
+    void f(vector<vector<int>>&matrix,vector<vector<int>>&vis,int n,int m){
+
+
+      for(int i=n-1;i>=0;i--){
+        for(int j=m-1;j>=0;j--){
+          if(matrix[i][j]==0){
+            vis[i][j]=0;
+          }
+        }
       }
-     
+
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(vis[i][j]==0){
+            for(int r=0;r<n;r++){
+              matrix[r][j]=0;
+            }
+
+            for(int c=0;c<m;c++){
+              matrix[i][c]=0;
+            }
+          }
+        }
+      }
+
+
+    }
 };
