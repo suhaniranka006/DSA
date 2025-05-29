5ab5809b850cfad02cfb341e537f186e5c23813c
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-05-29 11:28:59 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,66 +1,14 @@
 class Solution {
 public:
-    void sortColors(vector<int>&nums) {
-       
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
 
-            //brute force
-    //             int size=nums.size();
-
-    //         int *arr=new int[size];
-    //     int k=0;
-    //     for(int i=0;i<nums.size();i++){
-    //         if(nums[i]==0){
-    //             arr[k]=0;
-    //             k++;
-    //         }
-    //     }
-        
-    //     for(int i=0;i<nums.size();i++){
-    //         if(nums[i]==1){
-    //             arr[k]=1;
-    //             k++;
-    //         }
-    //     }
-    
-    //   for(int i=0;i<nums.size();i++){
-    //         if(nums[i]==2){
-    //             arr[k]=2;
-    //             k++;
-    //         }
-    //     }
-
-    //     for(int i=0;i<nums.size();i++){
-    //         nums[i]=arr[i];
-    //     }
-
-    //     delete[] arr;
-    // }
-    //tc o(n)
-    //sc o(n)
-
-    //optimised approach
-    //bubble sort in place
-    // for(int i=0;i<nums.size();i++){
-    //     for(int j=0;j<nums.size()-1;j++){
-    //         if(nums[j]>nums[j+1]){
-    //             swap(nums[j],nums[j+1]);
-    //         }
-    //     }
-    // }
-    // }
-
-    //tc o (n^2)
-    //sc o(1)
-
-
-    for(int i=0;i<nums.size();i++){
-        for(int j=0;j<nums.size()-1;j++){
-            if(nums[j]>nums[j+1]){
-                swap(nums[j],nums[j+1]);
+        for(int i=0;i<n;i++){
+          for(int j=i+1;j<n;j++){
+            if(nums[i]>nums[j]){
+              swap(nums[i],nums[j]);
             }
+          }
         }
     }
-
-}
-    
 };
