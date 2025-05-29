1bc25bd439a6ee29e0fd616a6dd2bd1519cb8d90
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-05-29 11:26:24 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-       
-    // int max_so_far = INT_MIN;
-    // int max_ending_here = 0;
+      //brute force 
+      // int n=nums.size();
+      // int ans=INT_MIN;
 
-    // for (int i = 0; i < nums.size(); i++) {
-    //     max_ending_here += nums[i];
-    //     if (max_so_far < max_ending_here) {
-    //         max_so_far = max_ending_here;
-    //     }
-    //     if (max_ending_here < 0) {
-    //         max_ending_here = 0;
-    //     }
-    // }
-    // return max_so_far;
-
-
-        //brute force
-        // int ans=INT_MIN;
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=0;j<nums.size();j++){
-        //         int sum=0;
-        //         for(int k =i;k<=j;k++){
-        //             sum+=nums[k];
-
-        //             if(sum>ans){
-        //                 ans=sum;
-        //             }
-        //         }
-        //     }
-        // }
-        // return ans;
+      // for(int i=0;i<n;i++){
+      //   for(int j=0;j<n;j++){
+      //     int sum=0;
+      //       for(int k=i;k<=j;k++){
+      //          sum+=nums[k];
+      //       }
+      //       if(sum>ans){
+      //         ans=sum;
+      //       }
+      //   }
+      // }
+      // return ans;
+      //not for -ve numbers
+      //tco (n^2)
 
 
-        //optimised
+      //better 
+      //kadane algo 
+      int n=nums.size();
+      int first=INT_MIN;
+      int sec=0;
 
-        //kadane algo 
-        int one=INT_MIN;
-        int sec=0;
-        for(int i=0;i<nums.size();i++){
-            sec+=nums[i];
-            if(one<sec){
-                one=sec;
-            }
-            if(sec<0){
-                sec=0;
-            }
+      for(int i=0;i<n;i++){
+        sec+=nums[i];
+        if(sec>first){
+          first=sec;
         }
-        return one;
+        if(sec<0){
+          sec=0;
+        }
+      }
+      return first;
+
+
+        
     }
 };
