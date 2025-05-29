64445e2807391f36c2edd9d94f66efe025a12092
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-05-29 10:50:12 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,95 +1,102 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        //brute force 
-        // 1.generate all permutations -- by recursion(sorted)
-        // 2.linear search for given permutation
-        // 3.find next permutation
+//     void nextPermutation(vector<int>& nums) {
+//         //brute force 
+//         // 1.generate all permutations -- by recursion(sorted)
+//         // 2.linear search for given permutation
+//         // 3.find next permutation
 
-        //t(n!*n)
-        //sc(o(n)+o(n))
+//         //t(n!*n)
+//         //sc(o(n)+o(n))
 
-        //better for cpp usrs
-        //we have inbuilt next_permutation stl
+//         //better for cpp usrs
+//         //we have inbuilt next_permutation stl
 
-        //next_permutation(nums.begin(),nums.end()); // by stl
+//         //next_permutation(nums.begin(),nums.end()); // by stl
 
-    //     //best
-    //     //apply stl 
-    //     //logest prefix match
-    //     //find >i but smallest one
-    //     //try to place remaining in sorted way
+//     //     //best
+//     //     //apply stl 
+//     //     //logest prefix match
+//     //     //find >i but smallest one
+//     //     //try to place remaining in sorted way
 
-    //     //find break point
-    //     int ind=-1;
-    //     int n=nums.size();
-    //     for(int i=n-2;i>=0;i--){
-    //         if(nums[i]>nums[i+1]){
-    //             ind=i; //break pt
-    //             break;
-    //         }
-    //     }
+//     //     //find break point
+//     //     int ind=-1;
+//     //     int n=nums.size();
+//     //     for(int i=n-2;i>=0;i--){
+//     //         if(nums[i]>nums[i+1]){
+//     //             ind=i; //break pt
+//     //             break;
+//     //         }
+//     //     }
 
-    //     //if break pt doent exist
-    //     if(ind==-1){
-    //         reverse(nums.begin(),nums.end());
+//     //     //if break pt doent exist
+//     //     if(ind==-1){
+//     //         reverse(nums.begin(),nums.end());
             
-    //     }
-    //     //find next greater eleemnt
-    //     for(int i=n-1;i>=0;i--){
-    //         if(nums[i]>nums[ind]){
-    //             swap(nums[i],nums[ind]);
-    //             break;
-    //         }
-    //     }
+//     //     }
+//     //     //find next greater eleemnt
+//     //     for(int i=n-1;i>=0;i--){
+//     //         if(nums[i]>nums[ind]){
+//     //             swap(nums[i],nums[ind]);
+//     //             break;
+//     //         }
+//     //     }
 
-    //     //reverse remaining
-    //     reverse(nums.begin()+ind+1,nums.end());
+//     //     //reverse remaining
+//     //     reverse(nums.begin()+ind+1,nums.end());
         
 
         
-    // }
+//     // }
 
-    solve(nums);
-    }
+//     solve(nums);
+//     }
 
 
-   void solve(vector<int>&nums){
-         //best
-        //apply stl 
-        //logest prefix match
-        //find >i but smallest one
-        //try to place remaining in sorted way
+//    void solve(vector<int>&nums){
+//          //best
+//         //apply stl 
+//         //logest prefix match
+//         //find >i but smallest one
+//         //try to place remaining in sorted way
 
-        //find break point
-        int ind=-1;
-        int n=nums.size();
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind=i; //break pt
-                break;
-            }
-        }
+//         //find break point
+//         int ind=-1;
+//         int n=nums.size();
+//         for(int i=n-2;i>=0;i--){
+//             if(nums[i]<nums[i+1]){
+//                 ind=i; //break pt
+//                 break;
+//             }
+//         }
 
-        //if break pt doent exist
-        if(ind==-1){
-            reverse(nums.begin(),nums.end());
-            return ;
+//         //if break pt doent exist
+//         if(ind==-1){
+//             reverse(nums.begin(),nums.end());
+//             return ;
             
-        }
-        //find next greater eleemnt
-        for(int i=n-1;i>ind;i--){
-            if(nums[i]>nums[ind]){
-                swap(nums[i],nums[ind]);
-                break;
-            }
-        }
+//         }
+//         //find next greater eleemnt
+//         for(int i=n-1;i>ind;i--){
+//             if(nums[i]>nums[ind]){
+//                 swap(nums[i],nums[ind]);
+//                 break;
+//             }
+//         }
 
-        //reverse remaining
-        reverse(nums.begin()+ind+1,nums.end());
+//         //reverse remaining
+//         reverse(nums.begin()+ind+1,nums.end());
         
-
+// //tc o(n)
+// //sc o(1)
         
-    }
+//     }
     
+     void nextPermutation(vector<int>& nums) {
+
+      next_permutation(nums.begin(),nums.end());
+
+
+     }
 };
