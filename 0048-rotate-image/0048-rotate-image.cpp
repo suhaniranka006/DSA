53ce1c87e74f0946730a4350a5b34ff8bf3336de
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-06-11 10:22:10 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.79%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,96 +1,21 @@
 class Solution {
 public:
-
- //  void rotate(vector<vector<int>>& matrix) {
-        //brute
-        //rc o(n^2)
-        //sc o(n^2)  
-        // int n=matrix.size();
-        // vector<vector<int>>temp(n,vector<int>(n,0));  
-        // solve(matrix,temp);
-
-
-        // //copy elemnts to original array
-
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         matrix[i][j]=temp[i][j];
-        //     }
-        // }
-
-
-        //optimal
-        //trnspose
-    //     int n=matrix.size();
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<i;j++){
-    //             swap(matrix[i][j],matrix[j][i]);
-    //         }
-    //     }
-
-    //     //reverse row
-    //     for(int i=0;i<n;i++){
-    //         reverse(matrix[i].begin(),matrix[i].end());
-    //     }
-    // }
-
-    //tc o(n*n)
-    //sc o(1)
-
-
-    
-    // void solve(vector<vector<int>>&matrix,vector<vector<int>>&temp){
-    //     int n=matrix.size();
-
-
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<n;j++){
-    //             temp[j][n-i-1]=matrix[i][j];
-
-    //         }
-    //     }
-    
-    // }
-
-
-    //brute force 
-    //tc o(m*n)
-    //sc o(m*n)
     void rotate(vector<vector<int>>& matrix) {
-       int rows=matrix.size();
-       int cols=matrix[0].size();
-      // vector<vector<int>>grid(rows,vector<int>(cols,-1));
+        int n=matrix[0].size();
+        int m=matrix.size();
 
-      // f(matrix,rows,cols,grid);
-
-      // //copy to original matrix
-      // for(int i=0;i<rows;i++){
-      //   for(int j=0;j<cols;j++){
-      //     matrix[i][j]=grid[i][j];
-      //   }
-      // }
-
-
-      //better approach
-      for(int i=0;i<rows;i++){
-        for(int j=0;j<i;j++){
-          swap(matrix[i][j],matrix[j][i]);
+        //transpose
+        for(int i=0;i<m;i++){
+          for(int j=0;j<i;j++){
+            swap(matrix[i][j],matrix[j][i]);
+          }
+          
         }
-      }
-        
-        //swap
-        for(int i=0;i<rows;i++){
+
+
+        //reverse row
+        for(int i=0;i<m;i++){
           reverse(matrix[i].begin(),matrix[i].end());
         }
     }
-
-
-    // void f(vector<vector<int>>&matrix,int rows,int cols,vector<vector<int>>&grid){
-
-    //   for(int i=0;i<rows;i++){
-    //     for(int j=0;j<cols;j++){
-    //       grid[j][cols-i-1]=matrix[i][j];
-    //     }
-    //   }
-    // }
 };
