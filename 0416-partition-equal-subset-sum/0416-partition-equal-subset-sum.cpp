372b9d61ed92b4da01457420e886c46007f2d303
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-06-12 12:43:33 +0530

    Time: 138 ms (43.2%), Space: 111.3 MB (15.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,128 +1,45 @@
 class Solution {
 public:
-    // bool canPartition(vector<int>& nums) {
-    //   int n=nums.size();
-    //   int sum=0;
-    //   for(int i=0;i<n;i++){
-    //     sum+=nums[i];
-    //   }
-
-    //   if(sum%2!=0){
-    //     return false;
-    //   }
-
-    //   return subset(nums,sum/2,n-1);
-        
-    // }
-
-    // bool subset(vector<int>&nums,int sum,int n){
-    //   if(n<0){
-    //     return false;
-    //   }
-    //   if(sum==0){
-    //     return true;
-    //   }
-
-    //   bool take=false;
-    //   if(nums[n]<=sum){
-    //     take =  subset(nums,sum-nums[n],n-1) ;
-    //   }
-    //   bool nontake = subset(nums,sum,n-1);
-
-    //  return take || nontake;
-    // }
-
-
-
-    //  bool canPartition(vector<int>& nums) {
-    //   int n=nums.size();
-    //   int sum=0;
-      
-    //   for(int i=0;i<n;i++){
-    //     sum+=nums[i];
-    //   }
-
-    //   if(sum%2!=0){
-    //     return false;
-    //   }
-
-    //     sum=sum/2;
-    //     vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-
-    //   return subset(nums,sum,n-1,dp);
-        
-    // }
-
-    // bool subset(vector<int>&nums,int sum,int n,vector<vector<int>>&dp){
-    
-
-    //   if(sum==0){
-    //     return true;
-    //   }
-    //   if(n<0){
-    //     return false;
-    //   }
-      
-
-    //   if(dp[n][sum]!=-1){
-    //       return dp[n][sum];
-    //   }
-    //   bool take=false;
-    //   if(nums[n]<=sum){
-    //     take =  subset(nums,sum-nums[n],n-1,dp) ;
-    //   }
-    //   bool nontake = subset(nums,sum,n-1,dp);
-
-    //  return dp[n][sum] =  take || nontake;
-    // }
-
-
-
     bool canPartition(vector<int>& nums) {
-      int n=nums.size();
+       
 
-      int sum=0;
-      for(int i=0;i<n;i++){
+       int n=nums.size();
+        int sum=0;
+       for(int i=0;i<n;i++){
         sum+=nums[i];
-      }
+       }
+       if(sum%2!=0){
+        return false;
+       }
+       int target=sum/2;
 
-      if(sum%2!=0){
+          vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+       return f(nums,n-1,sum/2,dp);
+    }
+
+    bool f(vector<int>&nums,int idx,int target,vector<vector<int>>&dp){
+
+
+      if(target==0){
+        return true;
+      }
+      if(idx<0){
         return false;
       }
 
-      sum = sum/2;
 
-      vector<vector<bool>>dp(n,vector<bool>(sum+1,0));
-
-
-      for(int i=0;i<n;i++){
-        dp[i][0]=true;
+      if(dp[idx][target]!=-1){
+        return dp[idx][target];
       }
+      bool nontake=f(nums,idx-1,target,dp);
 
-      if (nums[0] <= sum) {
-        dp[0][nums[0]] = true;
-    }
-
-      for(int i=1;i<n;i++){
-        for(int j=1;j<=sum;j++){
-        bool take = false; 
-
-        bool nontake = dp[i-1][j];
-
-        if(nums[i]<=j){
-          take = dp[i-1][j-nums[i]];
-        }
-
-        dp[i][j]= take || nontake;
-
-        }
+    bool take = false;
+      if(nums[idx]<=target){
+        take  = f(nums,idx-1,target-nums[idx],dp);
+        
 
       }
-
-      return dp[n-1][sum];
-
+      return dp[idx][target] =  take || nontake;
 
     }
-
-    
 };
