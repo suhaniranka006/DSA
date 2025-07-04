56296eb0e731b451b919577b6a0a2e83d7ad3003
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-07-04 18:48:45 +0530

    Time: 35 ms (90.16%), Space: 45.3 MB (69.81%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,25 +1,71 @@
 class Solution {
 public:
   int subarraySum(vector<int>& nums, int k) {
-    unordered_map<int, int> prefixCount;
-    int count = 0;
-    int sum = 0;
-    
-    prefixCount[0] = 1;  // To handle case when subarray itself starts from index 0
 
-    for (int i = 0; i < nums.size(); i++) {
-        sum += nums[i];
 
-        // If sum - k exists, add the number of times it occurred
-        if (prefixCount.find(sum - k) != prefixCount.end()) {
-            count += prefixCount[sum - k];
-        }
+    //brute force 
+    //tc o(n^2)
+    //sc o(1)
+    // int n=nums.size();
+  
+    // int cnt=0;
+    // for(int i=0;i<n;i++){
+    //     int sum=0;
+    //   for(int j=i;j<n;j++){
+    //     sum+=nums[i];
+    //     if(sum==k){
+    //       cnt++;
+    //     }
+    //   }
 
-        // Store/update current prefix sum
-        prefixCount[sum]++;
+    //   }
+    //   return cnt;
+
+    // }
+
+
+    //optmised 
+    //tc o(n^2)
+    //sc o(n)
+    unordered_map<int,int>mp;
+    int cnt=0;
+    int sum=0;
+    mp[0]=1;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+      sum+=nums[i];
+
+      //if sum-k seems before
+      if(mp.find(sum-k)!=mp.end()){
+        cnt+=mp[sum-k];
+      }
+      mp[sum]++;
+
+    }
+    return cnt;
     }
 
-    return count;
-}
+
+    // unordered_map<int, int> prefixCount;
+    // int count = 0;
+    // int sum = 0;
+    
+    // prefixCount[0] = 1;  // To handle case when subarray itself starts from index 0
+
+    // for (int i = 0; i < nums.size(); i++) {
+    //     sum += nums[i];
+
+    //     // If sum - k exists, add the number of times it occurred
+    //     if (prefixCount.find(sum - k) != prefixCount.end()) {
+    //         count += prefixCount[sum - k];
+    //     }
+
+    //     // Store/update current prefix sum
+    //     prefixCount[sum]++;
+    // }
+
+    // return count;
+  
+
 
 };
