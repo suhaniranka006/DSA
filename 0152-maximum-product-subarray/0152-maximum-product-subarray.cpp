bcc46e7beee349db542fcba3c56be8d17d54234e
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-07-16 23:36:28 +0530

    Time: 0 ms (100%), Space: 17.6 MB (84.42%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        // int n=nums.size();
+
+        // int maxi=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     int multi=1;
+        //     for(int j=i;j<n;j++){
+        //         multi*=nums[j];
+
+        //         maxi=max(maxi,multi);
+        //     }
+
+        // }
+        // return maxi;
+
+
+        //better
+
+            //kadane ago -- track max and min both to handle -ve numbers
         int n=nums.size();
 
-        int maxi=INT_MIN;
-        for(int i=0;i<n;i++){
-            int multi=1;
-            for(int j=i;j<n;j++){
-                multi*=nums[j];
+        int result=nums[0];
+        int maxi=nums[0];
+        int mini=nums[0];
 
-                maxi=max(maxi,multi);
+        for(int i=1;i<n;i++){
+
+            if(nums[i]<0){
+                swap(maxi,mini);
             }
 
+            maxi=max(nums[i],nums[i]*maxi);
+            mini=min(nums[i],nums[i]*mini);
+
+            result=max(result,maxi);
         }
-        return maxi;
+
+return result;
+
+
     }
 };
