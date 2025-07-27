fe0bd9f2cf2707cbb2079c8db6be2dd99ba04584
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-07-27 17:24:13 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.63%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,52 +1,36 @@
 class Solution {
 public:
-
-
     void nextPermutation(vector<int>& nums) {
-
-      //brute force
-      //reucrsion
-      //find all permutation 
-      //find current array then find next permutation of this
-      //tc o(n!*logn(n)) // to generate and sort
-
-      //better - stl
-     // next_permutation(nums.begin(),nums.end());
-     //tc o(n)
-     //sc o(1)
-
-     //best implementation
-     //find decreasing trend nums[i]<nums[i+1] 
-     //find nums[j]>nums[j-1]
-     //then swap nums[i],nums[j]
-     //then reverse suffix from i+1
-
-
-   //code 
-
-   //tc o(n)
-   //sc o(1)
-   int n=nums.size();
-   int i=n-2;
-   //generate i 
-   while(i>=0 && nums[i]>=nums[i+1]){
-    i--;
-   }
-
-   //generate j
-   if(i>=0){
-    int j=n-1;
-    while(nums[j]<=nums[i]){
-      j--;
-    }
-
-     swap(nums[i],nums[j]);
-   }
-
-  
-
-      //reverse
-      reverse(nums.begin()+i+1,nums.end());
         
+
+        int n=nums.size();
+
+
+        int ind=-1;
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+            //find just greater 
+
+            
+            for(int i=n-1;i>ind;i--){
+                if(nums[i]>nums[ind]){
+                    swap(nums[i],nums[ind]);
+                    break;
+
+                }
+            }
+
+            reverse(nums.begin()+ind+1,nums.end());
     }
 };
