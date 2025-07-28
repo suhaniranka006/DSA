6c9ed5b9a5036ec9b7052e077a99e0cbeebf5270
suhaniranka006 <117574469+suhaniranka006@users.noreply.github.com>
2025-07-28 16:23:45 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.66%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        
+        //dutch national flag
         int n=nums.size();
 
-        for(int i=0;i<n;i++){
-          for(int j=i+1;j<n;j++){
-            if(nums[i]>nums[j]){
-              swap(nums[i],nums[j]);
+        int low=0;
+        int high=n-1;
+        int mid=0;
+
+        while(mid<=high){
+
+
+
+                  if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
             }
-          }
+
+            
+           else if(nums[mid]==1){
+            
+             mid++;
+            }
+           else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+
+          
+
+
+            
         }
+        
     }
 };
